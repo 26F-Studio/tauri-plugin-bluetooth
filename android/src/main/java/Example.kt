8f9fd89aeb5846ee1d7a5b3ea231d9a08ce9c0c2
ParticleG <particle_g@outlook.com>
2025-02-12 03:13:30 +0800

    - Init project

---
+++ android/src/main/java/Example.kt
@@ -0,0 +1,10 @@
+package org.studio26f.tauri.plugin.bluetooth
+
+import android.util.Log
+
+class Example {
+    fun pong(value: String): String {
+        Log.i("Pong", value)
+        return value
+    }
+}
